d3831b910e0573bec5252194d47f3121cb96255f
winmaxim <68556857+winmaxim@users.noreply.github.com>
2020-12-02 22:43:17 -0500

    updated dockerbuild file

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -16,13 +16,19 @@ while [[ "$#" -gt 0 ]]
     esac
     shift
   done
+
 # Uses Current AWS Region if Not Supplied as Argument (Metadata Service & IAM Role Required)
 AWS_REGION=${AWS_REGION:-$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')}
+
 # Uses Current AWS Account ID if Not Supplied as Argument (Metadata Service & IAM Role Required)
 AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .accountId)}
+
 aws ecr get-login-password --region $AWS_REGION | sudo docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
 gradle build
 sudo service docker start
-sudo docker build -t fixengineonaws --file Dockerfile$ENGINE_ROLE
-sudo docker tag fixengineonaws:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME:latest
-sudo docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPOSITORY_NAME:latest
+sudo docker build -t fixengineonaws --file Dockerfile$ENGINE_ROLE .
+
+sudo docker tag fixengineonaws:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO_NAME:latest
+
+sudo docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$ECR_REPO_NAME:latest
